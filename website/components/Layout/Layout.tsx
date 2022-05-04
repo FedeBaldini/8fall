e74e7b9c8d20a348d9f7c754422e3d46a8d4efbe
Federico Baldini <55654811+FedeBaldini@users.noreply.github.com>
2022-05-04 11:49:51 +0200

    Add NFT generator server (#9)

--- website/components/Layout/Layout.tsx
+++ website/components/Layout/Layout.tsx
@@ -10,7 +10,9 @@ export function Layout({ children }: WithChildren) {
         alt="8 Fall"
         className="absolute top-0 left-1/2 -translate-x-1/2 -z-1 object-cover h-full md:object-fill md:h-auto"
       />
-      <div className="absolute left-0 right-0 pt-4 px-8">{children}</div>
+      <div className="absolute left-0 right-0 pt-4 px-4 md:px-8">
+        {children}
+      </div>
     </main>
   );
 }
