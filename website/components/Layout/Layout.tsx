31b028d4aff383116810bcf82c1785426ea9a6cc
Federico Baldini <federicobaldini@192.168.1.57>
2022-05-06 18:06:06 +0200

    add bg image

--- website/components/Layout/Layout.tsx
+++ website/components/Layout/Layout.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
-
 import { WithChildren } from "../../utils/types";
 import { Header } from "./Header";
 
 export function Layout({ children }: WithChildren) {
   return (
-    <main className="relative max-w-[120rem] my-0 mx-auto h-screen">
+    <main className="h-screen bg-[url('/background.png')] bg-cover bg-fixed bg-center">
       <Header />
-      <Image src="/background.png" alt="8 Fall" layout="fill" />
       <div className="absolute left-0 right-0 pt-8 px-4 md:px-8">
         {children}
       </div>
